b6f5444aecd2c753d312037b1cf945b879a16795
gopty <d.channyeinhan56@gmail.com>
2025-06-13 11:27:35 +0630

    updated flask

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # <-- this enables CORS for all routes
 
 @app.route('/api/burger')
 def get_burger():
-    return jsonify({"item": "burger", "price": 7000})
+    return jsonify({"item": "burger", "price": 5.99})
 
 @app.route('/api/hotdog')
 def get_hotdog():
-    return jsonify({"item": "hotdog", "price": 5000})
+    return jsonify({"item": "hotdog", "price": 3.99})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
